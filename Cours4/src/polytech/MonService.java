f29387c2b1fc8457a1902f3ea8a150104a27a1c2
Léo K <leokrebs68@gmail.com>
2021-01-25 17:50:31 +0100

    Delete wrong files

--- Cours4/src/polytech/MonService.java
+++
@@ -1,8 +0,0 @@
-import javax.jws.WebService;
-
-@WebService(targetNamespace = "http://polytech.nancy.fr")
-public class MonService {
-    public double Convertir(double mt) {
-        return mt*1.6;
-    }
-}
--- Cours4/src/polytech/Product.java
+++
@@ -1,2 +0,0 @@
-package polytech;public class Product {
-}
