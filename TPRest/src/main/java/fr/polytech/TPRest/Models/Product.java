0d986b55f20bcc85d63ea1a05aaf1d6fcbbbc273
Léo K <leokrebs68@gmail.com>
2021-02-03 14:43:17 +0100

    Add exercices cours 6

---
+++ TPRest/src/main/java/fr/polytech/TPRest/Models/Product.java
@@ -0,0 +1,39 @@
+package fr.polytech.TPRest.Models;
+
+public class Product {
+    private int code;
+    private String designation;
+    private double prix;
+
+    public Product() { super(); }
+
+    public Product(int code, String designation, double prix) {
+        this.code = code;
+        this.designation = designation;
+        this.prix = prix;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+}
--- TPRest/src/main/java/fr/polytech/TPRest/Servlet/HelloServlet.java
+++ TPRest/src/main/java/fr/polytech/TPRest/Servlet/HelloServlet.java
@@ -3,8 +3,13 @@ package fr.polytech.TPRest.Servlet;
 
 
 import fr.polytech.TPRest.Models.Person;
+import fr.polytech.TPRest.Models.Pokemon;
+import fr.polytech.TPRest.Models.Product;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.print.attribute.standard.Media;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -15,6 +20,14 @@ import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class HelloServlet extends HttpServlet {
+
+    static List<Product> liste = new ArrayList<>();
+    static {
+        liste.add(new Product(1,"test1",50));
+        liste.add(new Product(2,"test2", 60));
+        liste.add(new Product(3,"test3", 70));
+        liste.add(new Product(4,"test4",80));
+    }
 
     @GET
     @Path("sayHello")
@@ -38,6 +51,26 @@ public class HelloServlet extends HttpServlet {
         Person p = new Person("nom","prenom");
         // Ici on peut récupérer par exemple la personne avec son nom, ou son id,...
         return Response.ok().entity(p).build();
+    }
+
+    @POST
+    @Path("ajouterunproduit/{code}/{designation}/{prix}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Product ajouterUnProduit(@PathParam("code") int code, @PathParam("designation") String designation, @PathParam("prix") double prix) {
+        Product p = new Product(code,designation,prix);
+        liste.add(p);
+        return p;
+    }
+
+    // ecrire une methode permettant d'ajouter un produit passé au format json et le renvoyer en xml
+
+    @POST
+    @Path("ajouterunproduitBis")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_XML)
+    public Product ajouterUnProduitBis(Product p){
+        liste.add(p);
+        return p;
     }
 
     @PUT
