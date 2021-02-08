264ae7a4672b23a5f2517742f977a505200a20f6
Léo B <leodu39200@gmail.com>
2021-02-08 17:56:38 +0100

    Update ServletClient.java

--- TPFinalRest/src/main/java/com/polytech/TPFinalRest/Servlet/ServletClient.java
+++ TPFinalRest/src/main/java/com/polytech/TPFinalRest/Servlet/ServletClient.java
@@ -19,4 +19,31 @@ public class ServletClient extends HttpServlet {
         ClientManager cm = new ClientManager();
         return cm.getAll();
     }
+
+    @POST
+    @Path("clients")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Client createClientDB(Client c) {
+        ClientManager cm = new ClientManager();
+        return cm.create(c);
+    }
+
+    @PUT
+    @Path("clients")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Client updateClientDB(Client c){
+        ClientManager cm = new ClientManager();
+        return cm.update(c);
+    }
+
+    @DELETE
+    @Path("clients")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response deleteClient(Client client)
+    {
+        ClientManager cm = new ClientManager();
+        return cm.delete(client);
+    }
 }
